ee3ab4d966bea78e3d2ba953eaec1e4fcc2b742a
svbais <fizats@inbox.lv>
2018-02-26 04:22:56 +0200

    jauns update no 26/02/2018

--- database/tools.py
+++ database/tools.py
@@ -15,43 +15,44 @@ class ActiveSubscription(object):
 
 # CONSTRUCTOR
     def __init__(self, cli):
-       # 1.
+       # 1. atlasa visus ended = False, frozen = True --> EXIT
         subscriptions = Abonementi.objects.filter( client = cli, ended = False, frozen = True )
         if subscriptions.count() > 0:
             return
-       # 2.
+       # 2. atlasa visus ended = False, active = True, secība sākot no senāk pirktā
         subscriptions = Abonementi.objects.filter( client = cli, ended = False, active = True ).order_by('purchase_date')
-       # 3.
+       # 3. pēc kārtas pārbauda vai der tagad un tūlīt, ja jā --> active, exist=True
         for s in subscriptions:
             temp = self.test( s )
             if temp == True:
                 self.active = s
                 self.exists = True
                 return
-       # 4.
+       # 4. atlasa visus ended = False, active = False, secība sākot no senāk pirktā
         subscriptions = Abonementi.objects.filter( client = cli, ended = False, active = False ).order_by('purchase_date')
-       # 5.
+       # 5. pēc kārtas pārbauda vai der tagad un tūlīt, ja jā --> active, exist=True
         for s in subscriptions:
             temp = self.test( s )
             if temp == True:
                 self.active = s
                 self.exists = True
                 return
-       # 6.
+       # 6. exist = False, active = [] (jeb self.x neko nemaina)
+
 
 # Testa funkcija
     def test(self, obj):
-       # 1.
+       # 1. nosaka datetime un laiku
         today = datetime.datetime.now()
         time = datetime.time()
-       # 2.
+       # 2. nosaka weekday nummuru
         day = today.weekday()
 
-       # 3.
+       # 3. nav ne laika limiti, ne reizes un nav ended --> return True
         if obj.subscr.time_limit == False and obj.subscr.times == False:
             return True
 
-       # 4. laika limits = nav
+       # 4. nav laika limits, ja ir reižu skaita reizes, ja ir --> return True
         if obj.subscr.time_limit == False:
             if obj.subscr.times == True:
                 if obj.subscr.times_count > 0:
@@ -59,7 +60,7 @@ class ActiveSubscription(object):
 
         test_times = False # lai skripts veic pārbaudi uz reizēm...
 
-       # 5. laika limits = ir
+       # 5. laika limits ir, pārbauda darbadienu laiku limitus, ja ir --> uz reižu pārbaudi
         if day >= 0 and day <= 4:
             if obj.subscr.time_limit_type.weekday1 == True:
                 if obj.subscr.time_limit_type.weekday1_start_time < time < obj.subscr.weekday1_end_time:
@@ -68,13 +69,13 @@ class ActiveSubscription(object):
                 if obj.subscr.time_limit_type.weekday2_start_time < time < obj.subscr.weekday2_end_time:
                     test_times = True
 
-       # 6. laika limits = ir
+       # 6. laika limits ir, pārbauda brīvdienu laika limitu, ja ir --> uz reižu pārbaudi
         if day == 5 or day == 6:
             if obj.subscr.time_limit_type.weekend == True:
                 if obj.subscr.time_limit_type.weekend_start_time < time < obj.subscr.weekend_end_time:
                     test_times = True
 
-       # 4-ā veidīgs.
+       # 7. "reižu pārbaude" --> ja i reižu un reizes ir --> return True, ja nav reižu --> return True
         if test_times == True: # pārbaudam vai nav reižu abonements...
             if obj.subscr.times == True: # ir, tad skaitam vai ir reizes...
                 if obj.subscr.times_count > 0:
@@ -82,36 +83,5 @@ class ActiveSubscription(object):
             else: # nav reižu abonements (bet jau iepriekš noteikts ka iekļaujās laika limitos)
                 return True
 
-       # 7.
+       # 7. Kaut kāda hrena dēļ neder nekur :D
         return False
-
-
-"""
-KONSTRUKTORS
-1. atlas visus ended = False, frozen = True --> EXIT
-
-2. atlasa visus ended = False, active = True, secība sākot no senāk pirktā
-3. pēc kārtas pārbauda vai der tagad un tūlīt, ja jā --> active, exist=True <-- funkcija test
-4. atlasa visus ended = False, active = False, secība sākot no senāk pirktā
-5. pēc kārtas pārbauda vai der tagad un tūlīt, ja jā --> active, exist=True <-- funkcija test
-6. exist = False, active = [] (jeb self.x neko nemaina)
-
-TEST
-1. nosaka datetime.datetime.now()
-2. nosaka ^ weekday nummuru
-
-3. nav ne laika limiti, ne reizes un nav ended --> return True
-
-4. ja abonements.subscr.time_limit == False -->
-  --> abonements.subscr.times == True --> abonements.subscr.times_count > 0 tad --> return True
-
-5. ja weekday_nr 0...4 (darbadiena) -->
-  --> abonements.subscr.time_limit_type.weekday1 == True --> datetime.datetime.now() ietilpst iekš range --> 4-ā veidīgs
-  --> abonements.subscr.time_limit_type.weekday2 == True --> datetime.datetime.now() ietilpst iekš range --> 4-ā veidīgs
-
-6. ja weekday_nr == 5 or weekday_nr == 6 -->
-  --> abonements.subscr.time_limit_type.weekend == True --> datetime.datetime.now() ietilpst iekš range --> 4-ā veidīgs
-
-7. return False
-
-"""
